1cdc362799706144caca1ccd2ac4d622b89bc7bc
RiccardoVib <riccardo.simionato.vib@gmail.com>
2024-09-23 17:59:56 +0200

    Update LSTM.py

--- Deep_Learning_Conditioned_Modeling_Of_Optical_Compression/Code/LSTM.py
+++ Deep_Learning_Conditioned_Modeling_Of_Optical_Compression/Code/LSTM.py
@@ -30,7 +30,22 @@ import pickle
 
 #
 def trainLSTM(data_dir, epochs, seed=422, **kwargs):
-    ckpt_flag = kwargs.get('ckpt_flag', False)
+
+    
+    """
+      :param data_dir: the directory in which dataset are stored [string]
+      :param batch_size: the size of each batch [int]
+      :param learning_rate: the initial leanring rate [float]
+      :param units: the number of model's units [array of int]
+      :param model_save_dir: the directory in which models are stored [string]
+      :param save_folder: the directory in which the model will be saved [string]
+      :param inference: if True it skip the training and it compute only the inference [bool]
+      :param w_length: input size [int]
+      :param epochs: the number of epochs [int]
+      :param act: activation function [string]
+
+    """
+    
     b_size = kwargs.get('b_size', 128)
     learning_rate = kwargs.get('learning_rate', 0.001)
     units = kwargs.get('units', [32, 32])
@@ -46,7 +61,8 @@ def trainLSTM(data_dir, epochs, seed=422, **kwargs):
     if inference:
         file_scaler = open(os.path.normpath('/'.join([data_dir, 'scaler.pickle'])), 'rb')
         scaler = pickle.load(file_scaler)
-
+        
+    # load the data
     x, y, x_val, y_val, x_test, y_test, scaler, fs = get_data(data_dir=data_dir, w_length=w_length, inference=inference, scaler=scaler,
                                                                               seed=seed)
 
@@ -89,52 +105,56 @@ def trainLSTM(data_dir, epochs, seed=422, **kwargs):
         model.compile(loss='mse', metrics=['mse'], optimizer=opt)
     else:
         raise ValueError('Please pass loss_type as either MAE or MSE')
-
+     # define callbacks: where to store the weights
     callbacks = []
-    if ckpt_flag:
-        ckpt_path = os.path.normpath(os.path.join(model_save_dir, save_folder, 'Checkpoints', 'best', 'best.ckpt'))
-        ckpt_path_latest = os.path.normpath(
+  
+    ckpt_path = os.path.normpath(os.path.join(model_save_dir, save_folder, 'Checkpoints', 'best', 'best.ckpt'))
+    ckpt_path_latest = os.path.normpath(
             os.path.join(model_save_dir, save_folder, 'Checkpoints', 'latest', 'latest.ckpt'))
-        ckpt_dir = os.path.normpath(os.path.join(model_save_dir, save_folder, 'Checkpoints', 'best'))
-        ckpt_dir_latest = os.path.normpath(os.path.join(model_save_dir, save_folder, 'Checkpoints', 'latest'))
+    ckpt_dir = os.path.normpath(os.path.join(model_save_dir, save_folder, 'Checkpoints', 'best'))
+    ckpt_dir_latest = os.path.normpath(os.path.join(model_save_dir, save_folder, 'Checkpoints', 'latest'))
 
-        if not os.path.exists(os.path.dirname(ckpt_dir)):
-            os.makedirs(os.path.dirname(ckpt_dir))
-        if not os.path.exists(os.path.dirname(ckpt_dir_latest)):
-            os.makedirs(os.path.dirname(ckpt_dir_latest))
+    if not os.path.exists(os.path.dirname(ckpt_dir)):
+        os.makedirs(os.path.dirname(ckpt_dir))
+    if not os.path.exists(os.path.dirname(ckpt_dir_latest)):
+        os.makedirs(os.path.dirname(ckpt_dir_latest))
 
-        ckpt_callback = tf.keras.callbacks.ModelCheckpoint(filepath=ckpt_path, monitor='val_loss', mode='min',
+    ckpt_callback = tf.keras.callbacks.ModelCheckpoint(filepath=ckpt_path, monitor='val_loss', mode='min',
                                                            save_best_only=True, save_weights_only=True, verbose=1)
-        ckpt_callback_latest = tf.keras.callbacks.ModelCheckpoint(filepath=ckpt_path_latest, monitor='val_loss',
+    ckpt_callback_latest = tf.keras.callbacks.ModelCheckpoint(filepath=ckpt_path_latest, monitor='val_loss',
                                                                   mode='min',
                                                                   save_best_only=False, save_weights_only=True,
                                                                   verbose=1)
-        callbacks += [ckpt_callback, ckpt_callback_latest]
-        latest = tf.train.latest_checkpoint(ckpt_dir_latest)
-        if latest is not None:
-            print("Restored weights from {}".format(ckpt_dir))
-            model.load_weights(latest)
-            # start_epoch = int(latest.split('-')[-1].split('.')[0])
-            # print('Starting from epoch: ', start_epoch + 1)
-        else:
-            print("Initializing random weights.")
+    callbacks += [ckpt_callback, ckpt_callback_latest]
+    # load the weights of the last epoch, if any
+    last = tf.train.latest_checkpoint(ckpt_dir_latest)
+    if last is not None:
+        print("Restored weights from {}".format(ckpt_dir_latest))
+        model.load_weights(last)
+    else:
+        # if no weights are found,the weights are random generated
+        print("Initializing random weights.")
 
     early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='loss', min_delta=0.000001, patience=20,
-                                                               restore_best_weights=True, verbose=0)
+                                                               restore_best_weights=True,
+                                                               verbose=0)
     callbacks += [early_stopping_callback]
-
     # train
     if not inference:
         results = model.fit(x, y, batch_size=b_size, epochs=epochs, verbose=0,
                             validation_data=(x_val, y_val),
                             callbacks=callbacks)
 
-    if ckpt_flag:
-        best = tf.train.latest_checkpoint(ckpt_dir)
-        if best is not None:
-            print("Restored weights from {}".format(ckpt_dir))
-            model.load_weights(best)
+     # load the best weights of the model
+    best = tf.train.latest_checkpoint(ckpt_dir)
+    if best is not None:
+        print("Restored weights from {}".format(ckpt_dir))
+        model.load_weights(best).expect_partial()
+    else:
+        # if no weights are found, there is something wrong
+        print("Something is wrong.")
 
+     # compute test loss
     test_loss = model.evaluate(x_test, y_test, batch_size=b_size, verbose=0)
     print('Test Loss: ', test_loss)
     if inference:
@@ -148,34 +168,38 @@ def trainLSTM(data_dir, epochs, seed=422, **kwargs):
             'learning_rate': learning_rate,
             'opt_type': opt_type,
             'loss_type': loss_type,
-            'layers': layers,
-            'units': n_units,
+            'layers_enc': layers_enc,
+            'layers_dec': layers_dec,
+            'n_units_enc': n_units_enc,
+            'n_units_dec': n_units_dec,
             'w_length': w_length,
             # 'Train_loss': results.history['loss'],
             'Val_loss': results.history['val_loss']
         }
         print(results)
-
     if not inference:
-        if ckpt_flag:
-            with open(os.path.normpath('/'.join([model_save_dir, save_folder, 'results.txt'])), 'w') as f:
-                for key, value in results.items():
-                    print('\n', key, '  : ', value, file=f)
-                pickle.dump(results,
-                            open(os.path.normpath('/'.join([model_save_dir, save_folder, 'results.pkl'])), 'wb'))
-
+      
+        with open(os.path.normpath('/'.join([model_save_dir, save_folder, 'results.txt'])), 'w') as f:
+            for key, value in results.items():
+                print('\n', key, '  : ', value, file=f)
+            pickle.dump(results,
+                        open(os.path.normpath('/'.join([model_save_dir, save_folder, 'results.pkl'])), 'wb'))
+            
     if inference:
 
+        # predict the test set and save the results
+
         predictions = model.predict(x_test)
+
         print('GenerateWavLoss: ', model.evaluate(x_test, y_test, batch_size=b_size, verbose=0))
         predictions = scaler[0].inverse_transform(predictions)
         x_test = scaler[0].inverse_transform(x_test[:, :, 0])
-        y_test = scaler[0].inverse_transform(y_test)
+        y_gen = scaler[0].inverse_transform(y_test)
 
         predictions = predictions.reshape(-1)
         x_test = x_test.reshape(-1)
         y_test = y_test.reshape(-1)
-
+        
         # Define directories
         pred_name = 'LSTM_pred.wav'
         inp_name = 'LSTM_inp.wav'
@@ -200,9 +224,8 @@ def trainLSTM(data_dir, epochs, seed=422, **kwargs):
 
 
 if __name__ == '__main__':
-    data_dir = './Files'
-
-    seed = 422
+    data_dir = './Files' # data folder to dataset
+    seed = 422 # seed in case reproducibility is desired
     trainLSTM(data_dir=data_dir,
               model_save_dir='../TrainedModels',
               save_folder='LSTM',
